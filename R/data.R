97ab9c337c99911edc0ee459ee1a81bb7d6ff7c9
Hyeyoung Maeng <h.maeng@lse.ac.uk>
2018-08-29 18:30:02 +0000

    version 1.1.0

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' A dataset containing true regression coefficients for simulation
+#'
+#' True regression coefficients used for simulation in "Regularised forecasting via smooth-rough partitioning of the regression coefficients", H. Maeng and P. Fryzlewicz (2018), preprint.
+#'
+#'
+#' @docType data
+#' @usage data(truebeta)
+#' @format A matrix with 356 rows and 4 variables:
+#' \describe{
+#'   \item{Case 1}{true regression coefficients for case 1}
+#'   \item{Case 2}{true regression coefficients for case 2}
+#'   \item{Case 3}{true regression coefficients for case 3}
+#'   \item{Case 4}{true regression coefficients for case 4}
+#' }
+"truebeta"
